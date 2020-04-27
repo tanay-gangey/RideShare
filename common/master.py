1f52691ec1e84a8842ff7ee710e9736e0ce6d2a6
Tanay Gangey <tanay.gangey@gmail.com>
2020-04-27 13:50:36 +0530

    more docker changes

---
+++ common/master.py
@@ -0,0 +1,119 @@
+import collections
+import docker
+import json
+import pika
+import requests
+
+from datetime import datetime
+from flask import jsonify, request
+from model import Base, engine, Ride, Session, User
+from requests.models import Response
+from sqlalchemy import create_engine
+
+# Create tables in database
+Base.metadata.create_all(engine)
+session = Session()
+
+# Connecting to the RabbitMQ container
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='rmq'))
+channel = connection.channel()
+
+# ------------------------------------------------------------------------------------
+
+# Common Code [to Master & Slave]
+
+
+def checkHash(password):
+    if len(password) == 40:
+        password = password.lower()
+        charSet = {"a", "b", "c", "d", "e", "f"}
+        numSet = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+        for char in password:
+            if char not in charSet and char not in numSet:
+                return False
+        return True
+    return False
+
+
+def writeDB(req):
+    data = req.get_json()
+    # if the above line doesnt work try data = json.loads(req)
+    if data["table"] == "User":
+        # Add a new User
+        if data["caller"] == "addUser":
+            responseToReturn = Response()
+            if checkHash(data["password"]):
+                newUser = User(
+                    username=data["username"], password=data["password"])
+                session.add(newUser)
+                session.commit()
+                responseToReturn.status_code = 201
+            else:
+                responseToReturn.status_code = 400
+            return (responseToReturn.text, responseToReturn.status_code, responseToReturn.headers.items())
+        # Remove an existing User
+        elif data["caller"] == "removeUser":
+            session.query(User).filter_by(username=data["username"]).delete()
+            session.query(Ride).filter_by(created_by=data["username"]).delete()
+            session.commit()
+            responseToReturn = Response()
+            responseToReturn.status_code = 200
+            return (responseToReturn.text, responseToReturn.status_code, responseToReturn.headers.items())
+
+    elif data["table"] == "Ride":
+        # Add a new Ride
+        if data["caller"] == "createRide":
+            source = int(data["source"])
+            dest = int(data["destination"])
+            responseToReturn = Response()
+            noRows = 198
+            if source in range(1, noRows + 1) and dest in range(1, noRows + 1):
+                newRide = Ride(created_by=data["created_by"], username="", timestamp=data["timestamp"],
+                               source=source, destination=dest)
+                session.add(newRide)
+                session.commit()
+                responseToReturn.status_code = 201
+            else:
+                responseToReturn.status_code = 400
+            return (responseToReturn.text, responseToReturn.status_code, responseToReturn.headers.items())
+
+        elif data["caller"] == "joinRide":
+            rideExists = session.query(Ride).filter_by(ride_id=data["rideId"]).first()
+            if rideExists.username:
+                rideExists.username += ", " + data["username"]
+            else:
+                rideExists.username += data["username"]
+            session.commit()
+            responseToReturn = Response()
+            responseToReturn.status_code = 200
+            return (responseToReturn.text, responseToReturn.status_code, responseToReturn.headers.items())
+
+        elif data["caller"] == "deleteRide":
+            session.query(Ride).filter_by(ride_id=data["rideId"]).delete()
+            session.commit()
+            responseToReturn = Response()
+            responseToReturn.status_code = 200
+            return (responseToReturn.text, responseToReturn.status_code, responseToReturn.headers.items())
+
+
+# Wrapper for writeDB
+def writeWrap(ch, method, props, body):
+    writeResponse = writeDB(body)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    channel.basic_publish(exchange='syncQ', routing_key='', body=body)
+    ch.basic_publish(exchange='', routing_key=props.reply_to, properties=pika.BasicProperties(
+        correlation_id=props.correlation_id), body=str(writeResponse))
+    return writeResponse
+
+# -----------------------------------------------------------------------------------
+
+# Master Code
+# Consume from writeQ for Master
+
+channel.basic_qos(prefetch_count=1)
+channel.queue_declare(queue='writeQ', durable=True)
+channel.basic_consume(queue='writeQ', on_message_callback=writeWrap)
+channel.start_consuming()
+
+# -----------------------------------------------------------------------------------
---
+++ common/orchestrator.py
@@ -0,0 +1,83 @@
+import json
+import pika
+import requests
+import uuid
+
+from datetime import datetime
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+class readWriteReq:
+    def __init__(self, publishQueue):
+        self.publishQ = publishQueue
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='rmq'))
+        self.channel = self.connection.channel()
+        result = self.channel.queue_declare(queue='', durable=True)
+        self.callbackQ = result.method.queue
+        self.channel.basic_consume(
+            queue=self.callbackQ,
+            on_message_callback=self.onResponse,
+            auto_ack=True)
+
+    def onResponse(self, ch, method, props, body):
+        if self.corID == props.correlation_id:
+            self.response = body
+
+    def publish(self, query):
+        self.response = None
+        self.corID = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key=self.publishQ,
+            properties=pika.BasicProperties(
+                reply_to=self.callbackQ,
+                correlation_id=self.corID,
+                delivery_mode=2), 
+            body=query)
+        while self.response is None:
+            self.connection.process_data_events()
+        self.connection.close()
+        return self.response
+
+
+@app.route('/api/v1/db/read', methods=["POST"])
+def readDB():
+    response = None
+    if request.method == "POST":
+        data = request.get_json()
+        newReadReq = readWriteReq('readQ')
+        response = newReadReq.publish(data)
+        print("[x] Sent [Read] %r" % data)
+        return response, 200
+    return response, 405
+
+
+@app.route('/api/v1/db/write', methods=["POST"])
+def writeDB():
+    response = None
+    if request.method == "POST":
+        data = request.get_json()
+        newWriteReq = readWriteReq('writeQ')
+        response = newWriteReq.publish(data)
+        print("[x] Sent [Write] %r" % data)
+        return response, 200
+    return response, 405
+
+
+@app.route('/api/v1/db/clear', methods=["POST"])
+def clearDB():
+    response = None
+    if request.method == "POST":
+        data = request.get_json()
+        newClearReq = readWriteReq('writeQ')
+        response = newClearReq.publish(data)
+        print("[x] Sent [Clear] %r" % data)
+        return response, 200
+    return response, 405
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port='80')
